e9f11bf2114cc0c98857ecdc2e9acee3b6163fc5
abushe136 <amir.mesfin136@gmail.com>
2025-10-06 15:12:53 +0300

    cheac the  age og if condtion

---
+++ cheakage.py
@@ -0,0 +1,11 @@
+age = int(input("inter your age"))
+
+# if age > 18 :
+#     message = "Eligible"
+# else:
+#     message = "not Eligible"
+
+# print(message)
+
+message = "Eligible" if age >+ 18 else "not Eligible"
+print(message) 
