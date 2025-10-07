b5474d7630fc9c762d56ec609f730f4a052084a5
abushe136 <amir.mesfin136@gmail.com>
2025-10-07 11:19:03 +0300

    while loop

--- ifnot.py
+++ ifnot.py
@@ -3,7 +3,6 @@
 # student = input("student (yes/no): ").lower() == "yes"
 
 
-
 # if high_income or good_income and not student:
 #     print("Eligible for loan")
 # else:
@@ -11,17 +10,30 @@
 # print("done")
 
 
-for number in range(3):
-  print(number)
+# for number in range(3):
+#   print(number)
 
-for number in range(2, 12, 3):
-  print(number)
-  if number == 8:
-     print("successful")
-     break
-  else:
-     print("no Eligible")
-     
-for X in range(5):
-   for y in range(4):
-     print(f"{X}, {y}")
+# for number in range(2, 12, 3):
+#   print(number)(
+#   if number == 8:
+#      print("successful")
+#      break
+#   else:
+#      print("no Eligible")
+
+# for X in range(5):
+#    for y in range(4):
+#      print(f"{X}, {y}")
+
+# print(type(range(5)))
+
+# number = 100
+# while number > 0:
+#     print(number)
+#     number = number // 2
+   
+   
+command = ""
+while command != "quit" :
+  command = input(">")
+  print("ECHO", command)
