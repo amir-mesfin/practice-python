8c99434640567412b514dc141d5eb299466c2372
abushe136 <amir.mesfin136@gmail.com>
2025-10-14 22:22:53 +0300

    worke on the shoping cart

--- shopingCart.py
+++ shopingCart.py
@@ -3,4 +3,4 @@ price = float(input("what is the price? :"))
 quality = int(input("how many would you like?"))
 
 totalPrice = price * quality  
-print(f"you hve bought {quality} X {item} \n total price is {totalPrice}$")
+print(f"you hve bought {quality} X {item} \n total price is {totalPrice}$")   
