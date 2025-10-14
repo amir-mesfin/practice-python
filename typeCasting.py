9a7096e4d316a10b097068052439a55bcc74ad99
abushe136 <amir.mesfin136@gmail.com>
2025-10-14 20:30:44 +0300

    change the data type

---
+++ typeCasting.py
@@ -0,0 +1,18 @@
+name = "abushe"
+
+age = 56
+
+gba = 3.90
+
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(is_student))
+
+gba = int(gba)
+print(gba)
+
+
+gba = str(gba)
+print(gba)
