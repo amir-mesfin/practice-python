2b88ac17dd9633ea6f920d08381c8d963a187eb5
abushe136 <amir.mesfin136@gmail.com>
2025-10-14 20:14:49 +0300

    practice the python

--- cheakage.py
+++ cheakage.py
@@ -1,11 +1,21 @@
 age = int(input("inter your age"))
 
-# if age > 18 :
-#     message = "Eligible"
-# else:
-#     message = "not Eligible"
+if age > 18 :
+    message = "Eligible"
+else:
+    message = "not Eligible"
 
-# print(message)
+print(message)
 
 message = "Eligible" if age >+ 18 else "not Eligible"
 print(message) 
+
+
+first_name = " abushe"
+
+print(f" name : {first_name}")
+
+price = 45.67
+
+print(f"the price is ${price}"   
+      )
