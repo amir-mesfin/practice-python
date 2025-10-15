085be3eec6f1edc5f7958ef72cbf2d3d7f75eef2
abushe136 <amir.mesfin136@gmail.com>
2025-10-15 04:02:31 +0300

    math built functionality

--- math.py
+++ math.py
@@ -4,21 +4,21 @@ y = -34
 
 z = 89
 
-# print(round(x))
+print(round(x))
 
-# print(abs(y))
+print(abs(y))
 
-# result = pow(4,3)
+result = pow(4,3)
 
-# print(result)
+print(result)
 
-# print(max(x, y, z))
-# print(min(x, y, z, -89))
+print(max(x, y, z))
+print(min(x, y, z, -89))
 
 
 import math
 
-# print(math.ceil(89.001))
+print(math.ceil(89.001))
 
 a =  float(input("enter the side a : "))
 b =  float(input("enter the side b : "))
@@ -27,6 +27,6 @@ c =  pow(a, 2) + pow(b, 2)
 
 d = math.sqrt(c)
 
-print(f"the haypothesis of the  {d}")
+print(f"the haypothesis of the  {d}") 
 
 
