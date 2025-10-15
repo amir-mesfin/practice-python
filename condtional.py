167d0afdaeaa6d76bfc0f6117aff1535f04178d1
abushe136 <amir.mesfin136@gmail.com>
2025-10-15 11:04:19 +0300

    work on calculator

--- condtional.py
+++ condtional.py
@@ -12,14 +12,30 @@ operator = input(" Enter the operator (-, /, *,  %, and +)")
 
 num2 = float(input("please inter the number :  "))
 
+
 if operator == "-":
     result = round(num1 - num2)
+    print(f"the result {num1} {operator} {num2} = {result}")
+    
 elif operator == "/":
     result = round(num1 / num2)
+    print(f"the result {num1} {operator} {num2} = {result}")
+    
 elif operator == "+":
     result = num1 + num2
+    print(f"the result {num1} {operator} {num2} = {result}")
+    
 elif operator == "%":
     result = num1 % num2
-else:
+    print(f"the result {num1} {operator} {num2} = {result}")
+    
+elif operator == "*":
     result = num1 * num2
-print(f"the result {num1} {operator} {num2} = {result}")
+    print(f"the result {num1} {operator} {num2} = {result}")
+    
+else :
+    print(f"{operator} is not valid")  
+    
+  
+
+
