389b72b13adba4976445b5f7b2ed5f2cd7a2eaaf
abushe136 <amir.mesfin136@gmail.com>
2025-10-15 03:44:41 +0300

    find hypothesis

---
+++ math.py
@@ -0,0 +1,32 @@
+x = 3.99
+
+y = -34
+
+z = 89
+
+# print(round(x))
+
+# print(abs(y))
+
+# result = pow(4,3)
+
+# print(result)
+
+# print(max(x, y, z))
+# print(min(x, y, z, -89))
+
+
+import math
+
+# print(math.ceil(89.001))
+
+a =  float(input("enter the side a : "))
+b =  float(input("enter the side b : "))
+
+c =  pow(a, 2) + pow(b, 2)
+
+d = math.sqrt(c)
+
+print(f"the haypothesis of the  {d}")
+
+
