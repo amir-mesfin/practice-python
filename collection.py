04f686d6e15ebaa46da90c4eda74b4f07a721da4
abushe136 <amir.mesfin136@gmail.com>
2025-10-18 17:27:52 +0300

    practice collection

---
+++ collection.py
@@ -0,0 +1,45 @@
+# fruit = ["abushe", "khalid", "bereket", "tofa", "keli", "ahmed", "abdela"]
+
+# # print(fruit[::-1])
+
+# # for name in fruit:
+# #    print(name)
+
+# # print(help(fruit))
+# # print(len(fruit))
+
+# # print(dir(fruit))
+# # print("bererket" in fruit)
+# # fruit.insert(0,"abdusa")
+# # fruit.remove("tofa")
+# # fruit.sort()
+# # print("tofa"in fruit)
+# # fruit.reverse()
+
+
+# print(fruit.index("abushe"))
+
+# print(fruit)
+
+
+name = {"abushe", "khalid", "bereket", "tofa", "keli", "ahmed", "abdela"}
+# print(name)
+# print(dir(name))
+# print(len(name))
+# print("abushe" in name)
+
+# name.add("bellew")
+# name.pop()
+# name.clear()
+# print(name)
+
+
+name1 = ("abushe", "khalid", "bereket", "tofa", "tofa", "tofa", "keli", "ahmed", "abdela")
+
+print(len(name1))
+print("abushe" in name1)
+print(name1.index("tofa"))
+print(name1.count("tofa"))
+
+for name in name1:
+  print(name)
