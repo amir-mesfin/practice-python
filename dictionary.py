bd977afa07d6b1e851f27662e4a97d52fe6a353f
abushe136 <amir.mesfin136@gmail.com>
2025-10-19 03:30:01 +0300

    practice menu dictionaaty in python

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+capitals = {"usa":"washington",
+            "india":"new delhi",
+            "china":"beijing"}
+
+# print(dir(capitals))
+# print(help(capitals))
+
+print(capitals.get("usa"))
+
+# capitals.update({"germany":"berlin"}) 
+# capitals.pop("india")
+# capitals.popitem() 
+# capitals.clear()
+# keys = capitals.keys()
+# values = capitals.values()
+
+# print(values)
+# print(keys)
+print(capitals)
