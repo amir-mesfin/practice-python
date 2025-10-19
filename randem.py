e8fa6c088173bb81697d5d0fefb154e45f7dc87d
abushe136 <amir.mesfin136@gmail.com>
2025-10-19 09:40:26 +0300

    practice random method

--- randem.py
+++ randem.py
@@ -1,5 +1,17 @@
 import random
 
-print(help(random))
+# print(help(random))
 
-random.ran
+# number = random.randint(1,6)
+
+# print(number)
+
+card = [1,2,4,5,6,78]
+
+print(random.choice(card))
+
+print(random.choices(card, k=3))
+
+print(random.shuffle(card))
+
+print(random.random())
