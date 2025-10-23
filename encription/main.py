888c7c3e5cb4d3935593c710f4e3a146cef82f44
amir-mesfin <amir.mesfin136@gmail.com>
2025-10-23 16:57:05 +0300

    feat: add initial implementation of encryption script

---
+++ encription/main.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+
+char_list = list(chars)
+
+key = char_list.copy()
+random.shuffle(key)
+print(f"char____{char_list}")
+print(f" key______{key}")
+
+# incription
+
+plain_text = input("enter your message to encript: ")#
+cipher_text = ""
+
+for letter in plain_text:
+    index = char_list.index(letter)
+    cipher_text += key[index]
+
+
+print(f"cipher text : {cipher_text}")
+print(f"orginal text : {plain_text}")
