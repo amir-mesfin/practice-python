d0da2283dbc09ebcfae43d9dd305f8cf03d3b3c2
amir-mesfin <amir.mesfin136@gmail.com>
2025-10-26 15:49:48 +0300

    worke on the  skelaton staructure part of the hang man gamwe

--- hangame/maim.py
+++ hangame/maim.py
@@ -3,3 +3,49 @@
 import random
 
 word = ("apple", "banana", "orange", "grape", "mango", "peach", "cherry", "strawberry", "watermelon")
+
+
+#  dictionary of the key
+#
+hangman_key = { 0: ("   ",
+                    "   ",
+                    "   ",),
+                1: (" o ",
+                    "   ",
+                    "   ",),
+                2: (" o ",
+                    " | ",
+                    "   ",),
+                3: (" o ",
+                    "/|",
+                    "   ",),
+                4: (" o ",
+                    "/|\\",
+                    "   ",),
+                5: (" o ",
+                    "/|\\",
+                    "/ ",),
+                6: (" o ",
+                    "/|\\",
+                    "/ \\",) }
+# print(hangman_key[0])
+
+# for line in hangman_key[6]:
+#     print(line)
+
+
+def display_man(wrong_guesses):
+    pass
+
+def display_hint(hint):
+    pass
+
+def display_ansewer(ansewer):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
