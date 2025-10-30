549b6778df58b95f07a22e66567c5aebd6736d90
abushe136 <amir.mesfin136@gmail.com>
2025-10-30 20:12:54 +0300

    work on th python practice

---
+++ object/Static.py
@@ -0,0 +1,25 @@
+class employee:
+    def __init__(self, name, position):
+      self.name = name
+      self,position = position
+      
+  
+    def get_info(self):
+      return f"{self.name} = {self.position}"  
+    
+    
+    @staticmethod
+    def  is_valid_position(position):
+        valid_position = ["manager", "cashier", "cook", "janitor"]
+        return position in valid_position
+      
+  
+  
+# print(employee.is_valid_position("cook"))
+   
+employee1 = employee("abushe", "manager")
+employee2 = employee("ahmed", "cashier")
+employee3 = employee("abdellah", "janitor")
+
+
+
