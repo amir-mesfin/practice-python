83630db3d2671755e6f08b4d71461642f795043b
amir-mesfin <amir.mesfin136@gmail.com>
2025-11-02 13:40:37 +0300

    Refactor file reading functions to improve error handling and fix JSON file path typo

--- files/readFile.py
+++ files/readFile.py
@@ -18,11 +18,28 @@ def read_text_file():
     if not os.path.exists(text_path):
         print(f"❌ Text file not found at {text_path}")
         return
+    try:
+            with open(text_path, 'r') as file:
+                content = file.read().strip()
+                print("📄 Text File Content:\n")
+                print(content if content else "(File is empty)")
+    except Exception as e:
+             print(f"❌ An error occurred while reading the text file: {e}")
 
-    with open(text_path, 'r') as file:
-        content = file.read().strip()
-        print("📄 Text File Content:\n")
-        print(content if content else "(File is empty)")
-
+# Function to read json file
+def read_json_file():
+    if not os.path.exists(json_path):
+        print(f"❌ JSON file not found at {json_path}")
+        return
+    try:
+        with open(json_pathm, 'r') as file:
+            data = json.load(file)
+            print("📄 JSON File Content:\n")
+            print(json.dumps(data, intend = 6))
+    except Exception as e:
+        print(f"❌ An error occurred while reading the JSON file: {e}")
 # Run function
+
+
 read_text_file()
+read_json_file()
