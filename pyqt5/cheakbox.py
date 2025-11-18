501f5a403b8d6b4e16f682181755c24587b6a36e
abushe136 <amir.mesfin136@gmail.com>
2025-11-18 22:28:09 +0300

    work on the  cheak box

---
+++ pyqt5/cheakbox.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Checkbox Example")
+        self.setGeometry(100, 100, 300, 200)
+
+        # Create a checkbox
+        self.checkbox = QCheckBox("Check me!", self)
+        self.checkbox.move(50, 50)
+        self.checkbox.stateChanged.connect(self.checkbox_changed)
+
+    def checkbox_changed(self, state):
+        if state:
+            print("Checked!")
+        else:
+            print("Unchecked!")
+
+# Main loop
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec())
