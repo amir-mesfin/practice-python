a590455faa577da97eab0b751f84e6b7f72c0637
abushe136 <amir.mesfin136@gmail.com>
2025-11-28 14:08:57 +0300

    weight work

--- reverse.py
+++ reverse.py
@@ -1,4 +1,10 @@
 weight = float(input("Enter Your weight"))
 unit = input("Kilogram or Pounds")
 
-if unit =="k"
+if unit =="k":
+  weight = weight * 2.205
+  unit = "lbs "
+  print(f"Your weight is : {round(weight, 1)} {unit}")
+  
+  
+  
